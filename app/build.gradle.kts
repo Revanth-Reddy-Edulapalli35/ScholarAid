b593f2f9b573af8ce1b2317493a8ae31dff5442a
Revanth Reddy Edulapalli <s3495165tees@gmail.com>
2025-11-12 12:00:00 +0530

    Added Dependencies - Splash Api, Navigation, Hilt, Room, Retrofit, Coil, File IO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,10 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.hilt.ksp)
+    alias(libs.plugins.hilt.android)
+    alias(libs.plugins.room.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -39,6 +43,10 @@ android {
     buildFeatures {
         compose = true
     }
+
+    room{
+        schemaDirectory("$projectDir/schemas")
+    }
 }
 
 dependencies {
@@ -57,4 +65,49 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+
+    //Splash Api
+    implementation(libs.androidx.core.splashscreen)
+
+    //Compose Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    //Dagger Hilt
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Activity Compose for permission handling
+    implementation(libs.androidx.activity.compose)
+
+    // Coil for image loading
+    implementation(libs.coil.compose)
+
+    // For coroutines with Firebase
+    implementation(libs.kotlinx.coroutines.play.services)
+
+    // Supabase
+    implementation(libs.storage.kt)
+    implementation(libs.postgrest.kt)
+    implementation(libs.gotrue.kt)
+    implementation(libs.ktor.client.android)
+
+    // For file operations
+    implementation(libs.commons.io)
+
+    // shimmer effect
+    implementation(libs.compose.shimmer)
+
+    // Pull to Refresh
+    implementation(libs.accompanist.swiperefresh)
 }
